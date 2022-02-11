0934e88ace22b35e20705c25833d670c08d0b219
Serhii Feshchuk <jeoparrdy@gmail.com>
2022-02-11 15:25:16 +0200

    Initial commit

---
+++ src/main/java/com/bobocode/msscbeerservice/MsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bobocode.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerServiceApplication.class, args);
+    }
+
+}
